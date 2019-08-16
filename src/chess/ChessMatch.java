b3fd022d08521ef55c73883c1f488390fab32ef7
Kenji <caio_yariwake@hotmail.com>
2019-08-16 14:54:23 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao existe peca na posicao de origem");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao existe movimentos possiveis para a peca escolhida");
+		}
 	}
 	
 	//Conversão de letra+numero para matriz
